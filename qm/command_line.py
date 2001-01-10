8767b1f4b75db0e391d0ac8ed6b09a22cdddbdbd
Benjamin Chelf <chelf@codesourcery.com>
2001-01-10 03:06:04 +0000

    2001-01-09  Benjamin Chelf  <chelf@mustangben.stanford.edu>
    	* qm/command_line.py: New file.
    
    	* qm/test.py (test_parser_create): Define.
    	(test_paresr_parse): Likewise.
    	(test_parser_help): Likewise.
    	(test_parser_duplicate_long): Likewise.
    	(test_parser_duplicate_short): Likewise.
    	(test_parser_two_char_short): Likewise.
    	(test_parser_no_long): Likewise.
    	(test_parser_dup_command_short): Likewise.
    	(test_parser_dup_command_long): Likewise.
    	(regession_tests): Add above tests.

---
+++ qm/command_line.py
@@ -0,0 +1,302 @@
+########################################################################
+#
+# File:   command_line.py
+# Author: Benjamin Chelf
+# Date:   2001-01-09
+#
+# Contents:
+#   Code for command line interface.
+#
+# Copyright (c) 2001 by CodeSourcery, LLC.  All rights reserved. 
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+########################################################################
+
+########################################################################
+#  Using the Command Parser
+#
+#  The command parser can be used by giving a list of flags and commands
+#  to be parsed. See the constructor below for the exact structure of
+#  those things. You can then use the parser to 1) generate help strings
+#  for the general program, 2) generate help strings for specific
+#  commands, and 3) parse command lines to split up which flags were
+#  passed, which command was given, and the arguments and flags to that
+#  command that were specified.
+#
+########################################################################
+
+
+########################################################################
+# imports
+########################################################################
+
+import getopt
+import string
+import copy
+
+########################################################################
+# classes
+########################################################################
+
+class CommandParser:
+    """Class for the functionality that parses the command line.
+
+    The command parser is used to easily specify a list of command line
+    options and commands to be parsed from an argument list."""
+    
+    def __init__(self, name, options, commands):
+        """Create a new command parser.
+
+        'name' -- The name of the executable that we are currently
+        using. This will normally be argv[0].
+        
+        'options' -- A list of 4-tuples specifying flags that you wish
+        this parser to accept. The 4-tuple has the following form:
+        (short_form, long_form, flags, description). 'short_form' must be
+        exactly one character. 'long_form' must be specified for every
+        flag in the list. 'arg_name' is a string representing the name of
+        the argument that is passed to this flag. If it is 'None,' then
+        this flag doesn't take an argument. 'description' is a string
+        describing the flag.
+
+        'commands' -- A list of 5-tuples specifying commands to be
+        accepted after the command line flags. The 5-tuple has the
+        following form: (name, short_description, args_string,
+        long_description, flags). 'name' is the string for the
+        command. 'short_description' is a short description of the command
+        to be printed out in general help. 'args_string' is the string
+        that will be printed after the command in the command specific
+        help. 'long_description' is the long description to be printed out
+        in the command specfic help. 'flags' is a list of 4-tuples of the
+        same form as the 'options' described above."""
+
+        self.__name = name
+        
+        # Check that the options are ok.
+        self.CheckOptions(options)
+        self.__options = copy.deepcopy(options)
+
+        self.__option_to_long = {}
+        for option in self.__options:
+            # Check for duplicate short options.
+            if self.__option_to_long.has_key('-' + option[0]):
+                raise ValueError, "Duplicate short option"
+            self.__option_to_long['-' + option[0]] = option[1]
+            # Check for duplicate long options.
+            if self.__option_to_long.has_key('--' + option[1]):
+                raise ValueError, "Duplicate long option"
+            self.__option_to_long['--' + option[1]] = option[1]
+        
+        # Check that the options for each command are ok.
+        for command in commands:
+            self.CheckOptions(command[4])
+
+        self.__commands = copy.deepcopy(commands)
+        for i in range(0, len(self.__commands)):
+            command = self.__commands[i]
+            map = {}
+            for option in command[4]:
+                # Check for duplicate short options.
+                if map.has_key('-' + option[0]):
+                    raise ValueError, "Duplicate short command option"
+                map['-' + option[0]] = option[1]
+                # Check for duplicate long options.
+                if map.has_key('--' + option[1]):
+                    raise ValueError, "Duplicate long command option"
+                map['--' + option[1]] = option[1]
+            command = command + (map,)
+            self.__commands[i] = command
+
+        # Build the options string for getopt.
+        self.__getopt_options = self.BuildGetoptString(self.__options)
+
+
+    def CheckOptions(self, options):
+        """Check that a list of options 4-tuples is correct.
+
+        'options' -- a list of 4-tuples as described above.
+
+        returns -- 1 if the options are all valid, 0 otherwise."""
+
+        for short_option, long_option, flags, descripton in options:
+            # Short form of the option must have exactly 1 character.
+            if short_option != None and len(short_option) != 1:
+                raise ValueError, "Short option must have exactly 1 character"
+            # Long form of the option must be specified.
+            if long_option == None or len(long_option) == 0:
+                raise ValueError, "Long option must be specified"
+        
+        return 1
+
+
+    def BuildGetoptList(self, options):
+        """Build a getopt list for the long options.
+
+        'options' -- a list of 4-tuples as described above.
+
+        returns -- a list to be passed to getopt to parse long options."""
+
+        # Build the options string for getopt
+        getopt_list = []
+
+        for option in options:
+            # Takes an argument
+            if option[2] != None:
+                getopt_list.append(option[1] + '=')
+            else:
+                getopt_list.append(option[1])
+
+        return getopt_list
+        
+        
+    def BuildGetoptString(self, options):
+        """Build a getopt string for the options passed in.
+
+        'options' -- a list of 4-tuples as described above.
+
+        returns -- a string to be passed to getopt to parse the
+        options."""
+
+        # Build the options string for getopt
+        getopt_string = ''
+
+        for option in options:
+            getopt_string = getopt_string + option[0]
+            # Takes an argument
+            if option[2] != None:
+                getopt_string = getopt_string + ':'
+
+        return getopt_string
+
+
+    def GetOptionsHelp(self, options):
+        """Return a string that is the basic help for options.
+
+        options -- a list of options to get the help string for.
+
+        returns -- a string to be printed for the options."""
+
+        help_string = ""
+
+        # Print out the short form, long form, and then the description.
+        for option in options:
+            help_add = ""
+            if option[0] == None:
+                help_add = "--%s\t\t: "%(option[1]) + option[3]
+            else:
+                help_add = "-%s, --%-15s\t\t: "%(option[0], option[1]) \
+                + option[3]
+            help_string = help_string + "\t%s\n"%(help_add)
+
+        return help_string
+
+        
+    def GetBasicHelp(self):
+        """Return a string that is the basic help for the commands.
+
+        returns -- a string to be printed with basic functionality of
+        arguments and commands."""
+
+        help_string = "Usage: %s"%(self.__name)
+        help_string = help_string \
+                      + "[ flags ] command [ command-flags ] [ arguments ]\n\n"
+        help_string = help_string + "Flags:\n"
+        help_string = help_string + self.GetOptionsHelp(self.__options)
+        help_string = help_string + "\nCommands:\n"
+        # Print out the commands and their short descriptions.
+        for command in self.__commands:
+            help_add = "%-18s\t\t\t: %s"%(command[0], command[1])
+            help_string = help_string + "\t%s\n"%(help_add)
+        help_string = help_string + "\n"
+
+        return help_string
+
+    
+    def GetCommandHelp(self, command):
+        """Return a string that is the help for a specific command.
+
+        command -- a string of the command that you want help for.
+
+        returns -- a string of help for a given command."""
+
+        help_string = "Usage: %s %s [ flags ] "%(self.__name, command)
+        for command_item in self.__commands:
+            if command_item[0] == command:
+                help_string = help_string + command_item[2] + "\n\n"
+                help_string = help_string + "Flags:\n"
+                help_string = help_string \
+                              + self.GetOptionsHelp(command_item[4])
+                help_string = help_string + "\n"
+                help_string = help_string + command_item[3] + "\n"
+                return help_string
+            
+        return "Command not found"
+
+        
+    def ParseCommandLine(self, argv):
+        """Parse a command line.
+
+        'argv' -- A string containing the command line starting with
+        argv[1]. Should not contain the name of the executed program.
+
+        returns -- A 4-tuple of the flags given, the command given, the
+        command flags, and the command arguments. Its form is this:
+        (flags, command, command_flags, command_args). 'flags' is a list
+        of 2-tuples indicating each flag specified and the argument given
+        to that flag (if applicable). 'command' is the command
+        given. 'command_flags' is a list of 2-tuples indicating each flag
+        given to the command and its possible argument. 'command-args' is
+        a list of arguments as given to the command."""
+
+        # Get the flags off of the front of the command line.
+        getopt_list = self.BuildGetoptList(self.__options)
+        flags, args = getopt.getopt(argv, self.__getopt_options,
+                                    getopt_list)
+        
+        for i in range(0, len(flags)):
+            flag = flags[i]
+            new_flag = (self.__option_to_long[flag[0]], flag[1])
+            flags[i] = new_flag
+        
+        # Get the command.
+        command = args[0]
+
+        # Get the arguments to the command.
+        args = string.join(args[1:])
+        command_options = []
+
+        for command_item in self.__commands:
+            if command_item[0] == command:
+                command_options = command_item[4]
+                break
+        getopt_string = self.BuildGetoptString(command_options)
+        getopt_list = self.BuildGetoptList(command_options)
+        command_flags, command_args = getopt.getopt(string.split(args),
+                                                    getopt_string,
+                                                    getopt_list)
+
+        for i in range(0, len(command_flags)):
+            flag = command_flags[i]
+            new_flag = (command_item[5][flag[0]], flag[1])
+            command_flags[i] = new_flag
+            
+        return (flags, command, command_flags, command_args)
--- qm/test.py
+++ qm/test.py
@@ -37,6 +37,8 @@
 
 import qm
 import qm.regression_test
+import qm.command_line
+import string
 
 ########################################################################
 # tests
@@ -62,9 +64,214 @@ def test_thunk_to_label():
            and qm.is_valid_label(qm.thunk_to_label("This is a test."))
 
 
+def test_parser_create():
+    """Tests the creation of a CommandParser object.
+
+    This will test the creation of a CommandParser by building a
+    data structure of various flags and commands and calling the
+    constructor CommandParser. It will only fail if that constructor
+    raises an exception."""
+    
+    global my_parser
+    name = 'qmtrack'
+    option_a = ('a', 'a_option', None, 'Option A Description')
+    option_b = ('b', 'b_option', 'foo', 'Option B Description')
+    option_c = ('c', 'c_option', None, 'Option C Description')
+    option_d = ('d', 'd_option', None, 'Option D Description')
+    option_e = ('e', 'e_option', None, 'Option E Description')
+    option_f = ('f', 'f_option', None, 'Option F Description')    
+    command_1 = ('create', 'Create an issue', 'name',
+                 "This command will create an issue. _name_ is the"
+                 " name of the issue to be created.",
+                 [option_d])
+    command_2 = ('edit', 'Edit an issue', 'issue field1=foo field2+=bar',
+                 "This command will edit an issue. _issue_ is the"
+                 " name of the issue to be edited. field1, field2,..."
+                 " are the fields to be edited.",
+                 [option_e])
+    command_3 = ('join', 'Join two issues', 'issue1 issue2',
+                 "This command will join two issues. _issue1_ and"
+                 " _issue2_ are the two issues to be joined.",
+                 [option_f])
+
+    options = [ option_a, option_b, option_c ]
+    commands = [ command_1, command_2, command_3 ]
+
+    my_parser = qm.command_line.CommandParser(name, options, commands)
+
+    return 1
+
+
+def test_parser_parse():
+    """Tests the parsing of the command line.
+
+    This will test the functionality of the parser by feeding it a command
+    line and verifying that the results that it returns are correct. It
+    will fail if either the return is incorrect or if the parser raises an
+    exception."""
+
+    command_line = '-b foo --a_option edit --e_option issue1 issue2'
+    command_line = string.split(command_line)
+
+    parsed_line = my_parser.ParseCommandLine(command_line)
+
+    if parsed_line[0][0][0] != 'b_option':
+        return 0
+    if parsed_line[0][0][1] != 'foo':
+        return 0
+    if parsed_line[0][1][0] != 'a_option':
+        return 0
+    if parsed_line[0][1][1] != '':
+        return 0
+    if parsed_line[1] != 'edit':
+        return 0;
+    if parsed_line[2][0][0] != 'e_option':
+        return 0;
+    if parsed_line[2][0][1] != '':
+        return 0;
+    if parsed_line[3][0] != 'issue1':
+        return 0;
+    if parsed_line[3][1] != 'issue2':
+        return 0;
+    return 1
+
+
+def test_parser_help():
+    """Tests the help generation functionality of the parser.
+
+    These tests will only fail if the parser functions to generate help
+    raise an exception while building the strings to return."""
+    
+    my_parser.GetBasicHelp()
+    my_parser.GetCommandHelp("create")
+    my_parser.GetCommandHelp("edit")
+    my_parser.GetCommandHelp("join")
+    return 1
+
+
+def test_parser_duplicate_long():
+    """Test to verify that dupicate long options are not allowed."""
+    
+    name = 'qmtrack'
+    option_a = ('a', 'a_option', None, 'Option A Description')
+    option_b = ('b', 'a_option', None, 'Option A Description')
+    option_c = ('a', 'c_option', None, 'Option A Description')    
+
+    try:
+        qm.command_line.CommandParser('qmtrack', [option_a, option_b], [])
+        return 0
+    except ValueError, msg:
+        if str(msg) != "Duplicate long option":
+            return 0
+        return 1
+
+    return 0
+
+
+def test_parser_duplicate_short():
+    """Test to verify that dupicate short options are not allowed."""
+    
+    name = 'qmtrack'
+    option_a = ('a', 'a_option', None, 'Option A Description')
+    option_b = ('b', 'a_option', None, 'Option A Description')
+    option_c = ('a', 'c_option', None, 'Option A Description')    
+
+    try:
+        qm.command_line.CommandParser('qmtrack', [option_a, option_c], [])
+        return 0
+    except ValueError, msg:
+        if str(msg) != "Duplicate short option":
+            return 0
+        return 1
+
+    return 0
+
+
+def test_parser_two_char_short():
+    """Test to verify that only 1 character short options are allowed."""
+    
+    name = 'qmtrack'
+    option_a = ('aa', 'a_option', None, 'Option A Description')
+
+    try:
+        qm.command_line.CommandParser('qmtrack', [option_a], [])
+        return 0
+    except ValueError, msg:
+        if str(msg) != "Short option must have exactly 1 character":
+            return 0
+        return 1
+
+    return 0
+
+
+def test_parser_no_long():
+    """Test to verify that long option format is required."""
+    
+    name = 'qmtrack'
+    option_a = ('a', '', None, 'Option A Description')
+
+    try:
+        qm.command_line.CommandParser('qmtrack', [option_a], [])
+        return 0
+    except ValueError, msg:
+        if str(msg) != "Long option must be specified":
+            return 0
+        return 1
+
+    return 0
+
+
+def test_parser_dup_command_short():
+    """Test to verify the short dup. command is not allowed in a command"""
+    
+    option_d = ('d', 'd_option', None, 'Option D Description')
+    option_e = ('d', 'e_option', None, 'Option E Description')
+    command_1 = ('create', 'Create an issue', 'name',
+                 "This command will create an issue. _name_ is the"
+                 " name of the issue to be created.",
+                 [option_d, option_e])
+    try:
+        qm.command_line.CommandParser('qmtrack', [], [command_1])
+        return 0
+    except ValueError, msg:
+        if str(msg) != "Duplicate short command option":
+            return 0
+        return 1
+
+    return 0
+
+
+def test_parser_dup_command_long():
+    """Test to verify the long dup. option is not allowed in a command."""
+    
+    option_d = ('d', 'd_option', None, 'Option D Description')
+    option_e = ('e', 'd_option', None, 'Option E Description')
+    command_1 = ('create', 'Create an issue', 'name',
+                 "This command will create an issue. _name_ is the"
+                 " name of the issue to be created.",
+                 [option_d, option_e])
+    try:
+        qm.command_line.CommandParser('qmtrack', [], [command_1])
+        return 0
+    except ValueError, msg:
+        if str(msg) != "Duplicate long command option":
+            return 0
+        return 1
+
+    return 0
+
 regression_tests = [
     test_is_valid_label,
     test_thunk_to_label,
+    test_parser_create,
+    test_parser_parse,
+    test_parser_help,
+    test_parser_duplicate_long,
+    test_parser_duplicate_short,
+    test_parser_two_char_short,
+    test_parser_no_long,
+    test_parser_dup_command_short,
+    test_parser_dup_command_long
     ]
 
 
