eb4b76b19e8931805dfba2475c597074ec2fd595
Zack Weinberg <zack@codesourcery.com>
2002-08-09 09:19:43 +0000

    i	Revised test suite.
    	* New files:
    	tests/QMTest/classes.qmc
    	tests/QMTest/selfdb.py
    	tests/QMTest/selftest.py
    	tests/regress/cycle1/QMTest/configuration
    	tests/regress/cycle1/a.qmt
    	tests/regress/cycle1/b.qmt
    	tests/regress/cycle1/results.qmr
    	tests/regress/cycle2/QMTest/configuration
    	tests/regress/cycle2/a.qmt
    	tests/regress/cycle2/b.qmt
    	tests/regress/cycle2/c.qmt
    	tests/regress/cycle2/results.qmr
    	tests/regress/cycle3/QMTest/configuration
    	tests/regress/cycle3/a.qmt
    	tests/regress/cycle3/b.qmt
    	tests/regress/cycle3/c.qmt
    	tests/regress/cycle3/results.qmr
    	tests/regress/cycle4/QMTest/configuration
    	tests/regress/cycle4/a.qmt
    	tests/regress/cycle4/b.qmt
    	tests/regress/cycle4/c.qmt
    	tests/regress/cycle4/d.qmt
    	tests/regress/cycle4/e.qmt
    	tests/regress/cycle4/f.qmt
    	tests/regress/cycle4/results.qmr
    	tests/regress/cycle5/QMTest/configuration
    	tests/regress/cycle5/a.qmt
    	tests/regress/cycle5/b.qmt
    	tests/regress/cycle5/results.qmr
    	tests/regress/gilles1/QMTest/configuration
    	tests/regress/gilles1/a.qmt
    	tests/regress/gilles1/b.qmt
    	tests/regress/gilles1/c.qmt
    	tests/regress/gilles1/results.qmr
    	tests/regress/noresource/QMTest/configuration
    	tests/regress/noresource/results.qmr
    	tests/regress/noresource/test.qmt
    	tests/regress/noresource/test2.qmt
    	tests/regress/xml1/QMTest/configuration
    	tests/regress/xml1/asdfasdf.qmt
    	tests/regress/xml1/fnord.qmt
    	tests/regress/xml1/results.qmr
    	tests/xmldb/.cvsignore
    	tests/xmldb/QMTest/configuration
    
    	* Renamed files from:
    	tests/unit.qms/common.qms/format_time1.qmt
    	tests/unit.qms/common.qms/graph_complete.qmt
    	tests/unit.qms/common.qms/graph_cycle.qmt
    	tests/unit.qms/common.qms/graph_pass.qmt
    	tests/unit.qms/common.qms/label_is_valid.qmt
    	tests/unit.qms/common.qms/label_thunk.qmt
    	tests/unit.qms/common.qms/parse_time1.qmt
    	tests/unit.qms/common.qms/parse_time2.qmt
    	tests/unit.qms/common.qms/parse_time3.qmt
    	tests/unit.qms/common.qms/parse_time4.qmt
    	tests/unit.qms/common.qms/parser_create.qmt
    	tests/unit.qms/common.qms/parser_dup_command_long.qmt
    	tests/unit.qms/common.qms/parser_dup_command_short.qmt
    	tests/unit.qms/common.qms/parser_help.qmt
    	tests/unit.qms/common.qms/parser_no_long.qmt
    	tests/unit.qms/common.qms/parser_parse.qmt
    	tests/unit.qms/common.qms/parser_two_char_short.qmt
    	tests/unit.qms/test.qms/classes.qms/scripttest1.qmt
    	tests/unit.qms/test.qms/classes.qms/tempdir1.qma
    	tests/unit.qms/test.qms/classes.qms/tempdir1.qmt
    
    	* to:
    	tests/xmldb/common.qms/format_time1.qmt
    	tests/xmldb/common.qms/graph_complete.qmt
    	tests/xmldb/common.qms/graph_cycle.qmt
    	tests/xmldb/common.qms/graph_pass.qmt
    	tests/xmldb/common.qms/label_is_valid.qmt
    	tests/xmldb/common.qms/label_thunk.qmt
    	tests/xmldb/common.qms/parse_time1.qmt
    	tests/xmldb/common.qms/parse_time2.qmt
    	tests/xmldb/common.qms/parse_time3.qmt
    	tests/xmldb/common.qms/parse_time4.qmt
    	tests/xmldb/common.qms/parser_create.qmt
    	tests/xmldb/common.qms/parser_dup_command_long.qmt
    	tests/xmldb/common.qms/parser_dup_command_short.qmt
    	tests/xmldb/common.qms/parser_help.qmt
    	tests/xmldb/common.qms/parser_no_long.qmt
    	tests/xmldb/common.qms/parser_parse.qmt
    	tests/xmldb/common.qms/parser_two_char_short.qmt
    	tests/xmldb/test.qms/classes.qms/scripttest1.qmt
    	tests/xmldb/test.qms/classes.qms/tempdir1.qma
    	tests/xmldb/test.qms/classes.qms/tempdir1.qmt
    
    	* Modified files:
    	GNUmakefile.in (check): Pass -c qmtest_path= to qmtest run.
    	tests/QMTest/configuration: Use QMSelftestDatabase.
    
    	* Deleted files:
    	tests/common.qms

---
+++ tests/QMTest/selftest.py
@@ -0,0 +1,105 @@
+########################################################################
+#
+# File:   testsuite/QMTest/database.py
+# Author: Zack Weinberg
+# Date:   2002-08-05
+#
+# Contents:
+#   Test database specific to QMTest self-test suite.
+#
+# Copyright (c) 2002 by CodeSourcery, LLC.  All rights reserved.
+#
+# For license terms see the file COPYING.
+#
+########################################################################
+
+########################################################################
+# imports
+########################################################################
+
+import os
+import re
+from   qm.test.test import *
+from   qm.test.result import *
+from   qm.platform import run_program_captured, ProgramTerminatedBySignalError
+
+########################################################################
+# classes
+########################################################################
+
+class RegTest(Test):
+    """A 'RegTest' performs a regression test on QMTest itself.
+
+    Each regression test is stored as a subdirectory of the 'regress'
+    directory.  Each such subdirectory is a complete test database in
+    itself, such that running "qmtest -D . run -O results.qmr" in that
+    directory should succeed, reporting all tests completed as
+    expected.  The test is judged to have succeeded if so."""
+
+    arguments = [
+        qm.fields.TextField(
+            name        = "path",
+            title       = "Path to test",
+            verbatim    = "true",
+            multiline   = "false",
+            description = """The pathname of the test.
+
+            This is a path to a directory in the file system containing
+            a complete, self-contained test database.  All the tests in
+            this database will be executed to perform the regression test."""
+            )
+        ]
+
+    # This pattern is matched against the output of a QMTest subprocess
+    # to determine if there were any unexpected outcomes.
+    good_outcome_pat = re.compile(
+        r"^-+\s+TESTS\s+WITH\s+UNEXPECTED\s+OUTCOMES\s+-+\s+None",
+        re.MULTILINE)
+
+    def Run(self, context, result):
+        path = self.path
+        results = os.path.join(path, "results.qmr")
+        output = os.path.join(path, "output.qmr")
+
+        # Sanity check the target location.
+        assert os.path.isdir(os.path.join(path, "QMTest"))
+        assert os.path.isfile(results)
+
+        # The QMTest binary to test is specified as a context variable.
+        qmtest = context['qmtest_path']
+
+        argv = (qmtest, "-D", path, "run", "-O", results, "-o", output)
+
+        try:
+            (status, stdout, stderr) = run_program_captured(qmtest, argv)
+        except ProgramTerminatedBySignalError:
+            # A fatal signal counts as a failure, not an error.
+            result.NoteException(cause="Child process received a fatal signal",
+                                 outcome=Result.FAIL)
+            return
+
+        result.Annotate({
+            "selftest.RegTest.cmdline"  : ' '.join(argv),
+            "selftest.RegTest.exitcode" : ("%d" % status),
+            "selftest.RegTest.stdout"   : stdout,
+            "selftest.RegTest.stderr"   : stderr
+            })
+
+        if stderr != '':
+            # Printing anything to stderr is a failure.
+            result.Fail("Child process reported errors")
+        elif status:
+            # Unsuccessful termination is a failure.  This is checked
+            # second because output on stderr should come along with
+            # an unsuccessful exit, and we want to pick the more specific
+            # failure cause.
+            result.Fail("Child process exited unsuccessfully")
+        elif self.good_outcome_pat.search(stdout) is None:
+            # If there were unexpected outcomes, it's a failure.
+            result.Fail("Unexpected outcomes reported")
+        else:
+            # Success.  Delete the uninteresting output file.
+            os.remove(output)
+
+
+        
