46ea7b08e9c6c913914d27f63289ac440f8565f7
Stefan Seefeld <stefan.seefeld@orthosoft.ca>
2003-08-28 20:28:30 +0000

    fix documentation and tidy up the code

--- setup.py
+++ setup.py
@@ -28,8 +28,14 @@ import glob
 def prefix(list, pref): return map(lambda x, p=pref: p + x, list)
 
 def find_file(paths, predicate):
-    """This function returns the found file (with path) or None if it
-    wasn't found"""
+    """Return a file satisfying 'predicate' from 'paths'.
+
+    'paths' -- A sequence of glob patterns.
+
+    'predicate' -- A callable taking a single string as an argument.
+
+    returns -- The name of the first file matching one of the 'paths'
+    and 'predicate'."""
     for path in paths:
         matches = filter(predicate, glob.glob(path))
         if matches:
@@ -37,150 +43,191 @@ def find_file(paths, predicate):
     return None
 
 class qm_build_doc(build.build):
-    """This class compiles the QMTest's documentation."""
+    """Defines the specific procedure to build QMTest's documentation.
+
+    As this command is only ever used on 'posix' platforms, no effort
+    has been made to make this code portable to other platforms such
+    as 'nt'."""
+
+    description = "build documentation"
 
     def call_jade(self, jade, args, dcl, type, src, builddir):
-        """The call_jade command compiles the output file from the
-        input files, using 'type' as the type attribute."""
+        """Runs 'jade' in a subprocess to process a docbook file.
+
+        'jade' -- The jade executable with its full path.
+
+        'args' -- A sequence of arguments to be passed.
+
+        'dcl' -- An sgml declaration file for xml.
+
+        'type' -- The output type to be generated.
+
+        'src' -- The xml (master) source file to be processed.
+
+        'builddir' -- The directory from which to call jade."""
         cwd = os.getcwd()
-        # just to be sure this is still valid after chdir()
+        # Use an absolute path so that calls to chdir do not invalidate
+        # the name.
         src = os.path.abspath(src)
         builddir = os.path.join(self.build_temp, builddir)
-        self.mkpath(builddir)
-        # the stylesheet defines the output dir, which is identical
-        # to the last component in 'builddir'. So we move one level
-        # up
-        os.chdir(os.path.dirname(builddir))
+        if (type == 'sgml'):
+            # The stylesheet used for html output sets
+            # 'html' to be the output directory. Jade
+            # expects that to exist.
+            self.mkpath(builddir + '/html')
+        else:
+            self.mkpath(builddir)
+            
+        os.chdir(builddir)            
         cmd = [jade] + args + ['-t', type]
         cmd += ['-d', os.path.join(cwd, 'doc', 'qm-%s.dsl'%type)]
-        if type == 'tex':
-            out = os.path.splitext(os.path.basename(src))[0] + '.tex'
-            cmd += ['-o', out]
         cmd += [dcl]
         cmd += [src]
+        self.announce(string.join(cmd, ' '))
         spawn(cmd)
-
-        if (type == 'sgml'):
-            tidy = find_executable('tidy')
-            if tidy:
-                for f in glob.glob('html/*.html'):
-                    spawn([tidy, '-wrap', '72', '-i', '--indent-spaces', '1',
-                           '-f', '/dev/null', '-asxml', '-modify', f])
-        elif (type == 'tex'):
-            # We have to change -- into -{-} so that TeX does not generate long 
-            # dashes.  This is a bug in Jade.
-            self.spawn(['sh', '-c',
-                        'sed -e "s|--|-{-}|g" < manual.tex > print/manual.tex'])
-            os.remove('manual.tex')
-
         os.chdir(cwd)
 
     def run(self):
+        """Run this command, i.e. do the actual document generation.
 
-        source_files = ['qm/test/doc/manual.xml',
-                        'qm/test/doc/introduction.xml',
-                        'qm/test/doc/tour.xml',
-                        'qm/test/doc/reference.xml']
+        As this command requires 'jade', it will do nothing if
+        that couldn't be found in the default path."""
+
+        source_files = map(os.path.normpath,
+                           ['qm/test/doc/manual.xml',
+                            'qm/test/doc/introduction.xml',
+                            'qm/test/doc/tour.xml',
+                            'qm/test/doc/reference.xml'])
 
         jade = find_executable('jade')
-        dcl = find_file(['/usr/share/doc/jade*/pubtext/xml.dcl',
-                         '/usr/share/doc/openjade*/pubtext/xml.dcl',
-                         '/usr/doc/jade*/pubtext/xml.dcl',
-                         '/usr/share/sgml/declaration/xml.dcl'],
+        dcl = find_file(map(os.path.normpath,
+                            ['/usr/share/doc/jade*/pubtext/xml.dcl',
+                             '/usr/share/doc/openjade*/pubtext/xml.dcl',
+                             '/usr/doc/jade*/pubtext/xml.dcl',
+                             '/usr/share/sgml/declaration/xml.dcl']),
                         os.path.isfile)
 
-        stylesheets = find_file(['/usr/lib/sgml/stylesheets/docbook',
-                                 '/usr/lib/sgml/stylesheets/dsssl/docbook',
-                                 '/usr/share/sgml/docbook/dsssl-stylesheets'],
+        stylesheets = find_file(map(os.path.normpath,
+                                    ['/usr/lib/sgml/stylesheets/docbook',
+                                     '/usr/lib/sgml/stylesheets/dsssl/docbook',
+                                     '/usr/share/sgml/docbook/dsssl-stylesheets']),
                                 os.path.isdir)
 
-        dtd = find_file(['/usr/lib/sgml',
-                         '/usr/share/sgml/docbook'],
+        dtd = find_file(map(os.path.normpath,
+                            ['/usr/lib/sgml',
+                             '/usr/share/sgml/docbook']),
                         os.path.isdir)
 
         if not jade or not dcl or not stylesheets or not dtd:
-            self.announce("can't build documentation")
+            self.warn("can't build documentation")
             return
 
         #
-        # build html output
+        # Build html output.
         #
-        self.announce("building html manual")
-        target = os.path.join(self.build_lib, 'qm/test/doc/html')
+        target = os.path.normpath(self.build_lib + '/qm/test/doc/html')
         if newer_group(source_files, target):
-            # remove first such that its mtime reflects this build
+            self.announce("building html manual")
+            # Remove the target first such that its new mtime reflects
+            # this build.
             if os.path.isdir(target): remove_tree(target)
             self.call_jade(jade, ['-D%s'%dtd, '-D%s'%stylesheets],
                            dcl, 'sgml',
-                           'qm/test/doc/manual.xml',
-                           'qm/test/doc/html')
+                           os.path.normpath('qm/test/doc/manual.xml'),
+                           os.path.normpath('qm/test/doc'))
+            tidy = find_executable('tidy')
+            if tidy:
+                for f in glob.glob(map(os.path.normpath,
+                                       self.build_temp + '/qm/test/doc/html/*.html')):
+                    spawn([tidy,
+                           '-wrap', '72', '-i',
+                           '--indent-spaces', '1',
+                           '-f', '/dev/null',
+                           '-asxml', '-modify', f])
             if self.build_temp != self.build_lib:
-                src = os.path.join(self.build_temp, 'qm/test/doc/html')
+                src = os.path.normpath(self.build_temp + '/qm/test/doc/html')
                 dst = target
                 self.mkpath(dst)
-                copy_tree(src, dst, 1, 1, 0, 1, self.verbose, self.dry_run)
+                copy_tree(src, dst, 1, 1, 0, 1,
+                          self.verbose, self.dry_run)
 
         #
-        # build tex output
+        # Build tex output.
         #
-        self.announce("building tex manual")
-        target = os.path.join(self.build_lib, 'qm/test/doc/print/manual.tex')
+        target = os.path.normpath(self.build_lib + '/qm/test/doc/print/manual.tex')
         if newer_group(source_files, target):
-            # remove first such that its mtime reflects this build
+            self.announce("building tex manual")
+            # Remove the target first such that its new mtime reflects
+            # this build.
             if os.path.isfile(target): os.remove(target)
-            # build tex output
+            self.call_jade(jade,
+                           ['-D%s'%dtd, '-D%s'%stylesheets, '-o', 'manual.tex'],
+                           dcl, 'tex',
+                           os.path.normpath('qm/test/doc/manual.xml'),
+                           os.path.normpath('qm/test/doc'))
+
             # Jade places the output TeX source file in the current directory,
             # so move it where we want it afterwards.
-            self.call_jade(jade, ['-D%s'%dtd, '-D%s'%stylesheets],
-                           dcl, 'tex',
-                           'qm/test/doc/manual.xml',
-                           'qm/test/doc/print')
-
+            # We have to change -- into -{-} so that TeX does not generate long 
+            # dashes.  This is a bug in Jade.
+            cwd = os.getcwd()
+            self.mkpath(self.build_temp + '/qm/test/doc/print')
+            os.chdir(os.path.normpath(self.build_temp + '/qm/test/doc'))
+            self.spawn(['sh', '-c',
+                        'sed -e "s|--|-{-}|g" < manual.tex > print/manual.tex'])
+            os.remove('manual.tex')
+            os.chdir(cwd)
             if self.build_temp != self.build_lib:
-                src = os.path.join(self.build_temp, 'qm/test/doc/print/manual.tex')
+                src = os.path.normpath(self.build_temp + '/qm/test/doc/print/manual.tex')
                 dst = target
                 self.mkpath(os.path.dirname(dst))
                 copy_file(src, target,
                           1, 1, 1, None, self.verbose, self.dry_run)
 
         #
-        # build pdf output
+        # Build pdf output.
         #
-        self.announce("building pdf manual")
-        target = os.path.join(self.build_lib, 'qm/test/doc/print/manual.pdf')
+        target = os.path.normpath(self.build_lib + '/qm/test/doc/print/manual.pdf')
         if newer_group(source_files, target):
-            # remove first such that its mtime reflects this build
+            self.announce("building pdf manual")
+            # Remove the target first such that its new mtime reflects
+            # this build.
             if os.path.isfile(target): os.remove(target)
             cwd = os.getcwd()
-            os.chdir(os.path.join(self.build_temp, 'qm/test/doc/print/'))
+            os.chdir(os.path.normpath(self.build_temp + '/qm/test/doc/print/'))
             self.spawn(['pdfjadetex', 'manual.tex'])
             self.spawn(['pdfjadetex', 'manual.tex'])
             self.spawn(['pdfjadetex', 'manual.tex'])
             os.chdir(cwd)
             if self.build_temp != self.build_lib:
-                src = os.path.join(self.build_temp, 'qm/test/doc/print/manual.pdf')
+                src = os.path.normpath(self.build_temp + '/qm/test/doc/print/manual.pdf')
                 dst = target
                 self.mkpath(os.path.dirname(dst))
                 copy_file(src, target,
                           1, 1, 1, None, self.verbose, self.dry_run)
 
+        #
+        # Build reference manual via 'happydoc'.
+        #
         happydoc = find_executable('happydoc')
         if (happydoc):
             self.announce("building reference manual")
             spawn(['happydoc', 'qm'])
 
 class qm_build(build.build):
-    """The qm_build class extends the build subcommands by 'qm_build_doc'."""
+    """Extends 'build' by adding support for building documentation."""
 
     sub_commands = build.build.sub_commands[:] + [('build_doc', None)]
 
 class qm_install_data(install_data.install_data):
-    """This class overrides the system install_data command. In addition
-    to the original processing, a 'config' module is created that
-    contains the data only available at installation time, such as
-    installation paths."""
+    """Extends 'install_data' by generating a config module.
+
+    This module contains data only available at installation time,
+    such as installation paths for data files."""
 
     def run(self):
+        """Run this command."""
+        
         id = self.distribution.get_command_obj('install_data')
         il = self.distribution.get_command_obj('install_lib')
         install_data.install_data.run(self)
@@ -206,7 +253,8 @@ packages=['qm',
           'qm/test',
           'qm/test/web']
 
-classes= filter(lambda f: f[-3:] == '.py', os.listdir('qm/test/classes/'))
+classes= filter(lambda f: f[-3:] == '.py',
+                os.listdir(os.path.join('qm','test','classes')))
 classes.append('classes.qmc')
 
 diagnostics=['common.txt','common-help.txt']
