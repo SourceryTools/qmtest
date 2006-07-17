699a12ebe560acee2c89a5172d149e68b930416c
Stefan Seefeld <stefan@codesourcery.com>
2006-07-17 18:46:06 +0000

    Better support for suites in 'qmtest create' and 'qmtest ls'.

--- qm/test/cmdline.py
+++ qm/test/cmdline.py
@@ -31,6 +31,7 @@ from   qm.test.context import *
 from   qm.test.execution_engine import *
 from   qm.test.result_stream import ResultStream
 from   qm.test.runnable import Runnable
+from   qm.test.suite import Suite
 from   qm.test.report import ReportGenerator
 from   qm.test.classes.dir_run_database import *
 from   qm.trace import *
@@ -1053,9 +1054,9 @@ Valid formats are %s.
         if extension_id is not None:
             # Create the extension instance.  Objects derived from
             # Runnable require magic additional arguments.
-            if issubclass(extension_class, Runnable):
-                extras = { Runnable.EXTRA_ID : extension_id, 
-                           Runnable.EXTRA_DATABASE : database }
+            if issubclass(extension_class, (Runnable, Suite)):
+                extras = { extension_class.EXTRA_ID : extension_id, 
+                           extension_class.EXTRA_DATABASE : database }
             else:
                 extras = {}
             extension = extension_class(arguments, **extras)
@@ -1283,8 +1284,15 @@ Valid formats are %s.
             extension = database.GetExtension(arg)
             if not extension:
                 raise QMException, qm.error("no such ID", id = arg)
-            if isinstance(extension, qm.test.directory_suite.DirectorySuite):
-                extensions.update(database.GetExtensions(arg, recursive))
+            if isinstance(extension, qm.test.suite.Suite):
+                if recursive:
+                    test_ids, suite_ids = extension.GetAllTestAndSuiteIds()
+                    extensions.update([(i, database.GetExtension(i))
+                                       for i in test_ids + suite_ids])
+                else:
+                    ids = extension.GetTestIds() + extension.GetSuiteIds()
+                    extensions.update([(i, database.GetExtension(i))
+                                       for i in ids])
             else:
                 extensions[arg] = extension
 
