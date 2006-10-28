e74c4aa1fef0b6062a3204fee389da07780075b4
Stefan Seefeld <stefan@codesourcery.com>
2006-10-28 01:14:08 +0000

    Improve documentation generation.

--- qmdist/command/build_doc.py
+++ qmdist/command/build_doc.py
@@ -17,12 +17,9 @@ from distutils.command import build
 from distutils.spawn import spawn, find_executable
 from distutils.dep_util import newer, newer_group
 from distutils.dir_util import copy_tree, remove_tree
-from distutils.file_util import copy_file
-import os
-import os.path
-from   os.path import join, normpath
-import string
-import glob
+from distutils.file_util import copy_file, move_file
+import os, os.path
+from   shutil import *
 
 class build_doc(build.build):
     """Defines the specific procedure to build QMTest's documentation.
@@ -38,187 +35,72 @@ class build_doc(build.build):
         ("no-html", None, "do not generate HTML documentation"),
         ("pdf", None, "generate PDF documentation"),
         ("no-pdf", None, "do not generate PDF documentation"),
-        ("internal", None, "generate internal documentation"),
-        ("no-internal", None, "do not generate internal documentation"),
-        
-        ("jadeopts=", None, "options to pass to Jade"),
-        ("xmldcl=", None, "path to xml.dcl"),
-        ("dtddir=", None, "path to DocBook DTD directory"),
-        ("stydir=", None, "path to DSSSL stylesheet directory"),
+        ("ref-manual", None, "generate reference manual"),
+        ("no-ref-manual", None, "do not generate reference manual"),
         ]
 
-    boolean_options = [ "html", "pdf", "internal" ]
+    boolean_options = [ "html", "pdf", "ref-manual" ]
     negative_opt = { "no-html" : "html",
                      "no-pdf" : "pdf",
-                     "no-internal" : "internal" }
+                     "no-ref-manual" : "ref-manual" }
     
-    xml_dcl_paths = (
-        # Debian Sarge
-        '/usr/share/xml/declaration/xml.dcl',
-        # Debian Sarge
-        '/usr/share/sgml/declaration/xml.dcl',
-        # Red Hat 8.0, RHEL 3
-        '/usr/share/sgml/xml.dcl',
-        # Cygwin
-        '/usr/share/OpenSP/xml.dcl',
-    )
-    """Paths to search for the SGML declaration of XML.
-
-    The paths used to locate the SGML declaration of XML.  These paths
-    are searched in the order listed."""
-    
-    dtd_dirs = (
-        '/usr/lib/sgml',
-        # Debian Sarge
-        # Red Hat 8.0, RHEL 3
-        '/usr/share/sgml/docbook',
-        # Cygwin
-        '/usr/share/xml/docbook'
-        )
-    """Directories to search for the DocBook DTD.
-
-    A list of directories searched for the DocBook DTD.  The
-    directories are searched in the order listed."""
-    
-    stylesheet_dirs = (
-        '/usr/lib/sgml/stylesheets/docbook',
-        '/usr/lib/sgml/stylesheets/dsssl/docbook',
-        # Red Hat 8.0, RHEL 3
-        '/usr/share/sgml/docbook/dsssl-stylesheets',
-        # Debian Sarge
-        '/usr/share/sgml/docbook/stylesheet/dsssl/modular'
-        )
-    """Directories to search for the DSSSL stylesheets.
-
-    A list of directories searched for the DSSSL stylesheets.  The
-    directories are searched in the order listed."""
-
     def initialize_options(self):
 
         self.html = True
         self.pdf = True
-        self.internal = True
-        
-        self.jadeopts = None
-        self.xmldcl = None
-        self.dtddir = None
-        self.stydir = None
-        
+        self.ref_manual = True
         build.build.initialize_options(self)
         
 
-    def finalize_options(self):
-
-        # Look for programs and supporting libraries required to build
-        # DocBook documentation.  Unfortunately, different
-        # distributions use different paths for the various key
-        # components.  We allow the user to tell us where to look on
-        # the command line.  If they do not provide a hint, then we
-        # look in locations known to be in use on various systems.
-        if not self.xmldcl:
-            self.xmldcl = self.find_file(map(normpath, self.xml_dcl_paths),
-                                         os.path.isfile,
-                                         "xml.dcl")
-        if not self.dtddir:
-            self.dtddir = self.find_file(map(normpath, self.dtd_dirs),
-                                         os.path.isdir,
-                                         "DocBook DTD")
-        if not self.stydir:
-            self.stydir = self.find_file(map(normpath, self.stylesheet_dirs),
-                                         os.path.isdir,
-                                         "DSSSL stylesheets")
-            
-        build.build.finalize_options(self)
-        
-        
-    def call_jade(self, jade, args, type, src, builddir):
-        """Runs 'jade' in a subprocess to process a docbook file.
-
-        'jade' -- The jade executable with its full path.
-
-        'args' -- A sequence of arguments to be passed.
-
-        'type' -- The output type to be generated.
-
-        'src' -- The xml (master) source file to be processed.
-
-        'builddir' -- The directory from which to call jade."""
-
-        # Use an absolute path so that calls to chdir do not invalidate
-        # the name.
-        src = os.path.abspath(src)
-        builddir = os.path.dirname(src)
-        if (type == 'sgml'):
-            # The stylesheet used for html output sets
-            # 'html' to be the output directory. Jade
-            # expects that to exist.
-            self.mkpath(builddir + '/html')
-        else:
-            self.mkpath(builddir)
-
-        cwd = os.getcwd()
-        os.chdir(builddir)            
-        cmd = [jade]
-        cmd += ['-D' + self.dtddir, '-D' + self.stydir]
-        cmd += args
-        cmd += ['-t', type]
-        if self.jadeopts:
-            cmd += self.jadeopts.split()
-        cmd += ['-d', os.path.join(cwd, 'doc', 'qm-%s.dsl'%type)]
-        cmd += [self.xmldcl]
-        cmd += [src]
-        self.announce(string.join(cmd, ' '))
-        spawn(cmd)
-        os.chdir(cwd)
-
-
     def run(self):
-        """Run this command, i.e. do the actual document generation.
-
-        As this command requires 'jade', it will do nothing if
-        that couldn't be found in the default path."""
+        """Run this command, i.e. do the actual document generation."""
 
         # If the user has not requested that we build any
         # documentation, just skip it.
-        if not self.html and not self.pdf:
+        if not self.html and not self.pdf and not self.ref_manual:
             return
         
-        source_files = map(normpath,
-                           ['doc/tutorial.xml',
-                            'doc/introduction.xml',
-                            'doc/concepts.xml',
-                            'doc/customizing.xml',
-                            'doc/extending.xml',
-                            'doc/cli_reference.xml'])
+        tempdir = os.path.abspath(os.path.join(self.build_temp, 'doc'))
+        srcdir = os.getcwd()
+        source_files = [os.path.abspath(p)
+                        for p in ['doc/tutorial.xml',
+                                  'doc/concepts.xml',
+                                  'doc/cli_reference.xml',
+                                  'doc/customizing.xml',
+                                  'doc/extending.xml']]
 
         # Look for programs and supporting libraries required to build
-        # DocBook documentation.  Unfortunately, different
-        # distributions use different paths for the various key
-        # components.  We allow the user to tell us where to look on
-        # the command line.  If they do not provide a hint, then we
-        # look in locations known to be in use on various systems.
+        # DocBook documentation.
         xsltproc = find_executable('xsltproc')
 
-        jade = find_executable('jade') or find_executable('openjade')
-        if not jade:
-            self.warn("could not find jade or openjade in PATH")
+        xep = False
+        foproc = find_executable('xep')
+        if foproc:
+            xsltproc += ' --stringparam xep.extensions 1'
+            xep = True
+        if not foproc:
+            foproc = find_executable('fop')
+        if not foproc:
+            foproc = find_executable('xmlroff')
+            if foproc: foproc += ' --compat'
+        if not foproc:
+            self.warn("could not find either of xep, fop, or xmlroff in PATH")
 
-        if not jade or not self.xmldcl or not self.stydir or not self.dtddir:
+        if not xsltproc or not foproc:
             self.warn("cannot build documentation")
             return
 
-        # All files that are generated below are generated in the
-        # source tree.  That is the only way that Distutils will
-        # install the documentation as data files (in "share") rather
-        # than as program files (in "lib").
-        
+        self.mkpath(tempdir)
+        os.chdir(tempdir)
+
+
         # 
         # Write the version to a file so the tutorial can refer to it.  This
         # file contains exactly the version number -- there must be no
         # trailing newline, for example.
         #
         self.announce("writing version file")
-        f = open(os.path.join("doc", "qm-version"), "w")
+        f = open(os.path.join('qm-version'), 'w')
         f.write(self.distribution.get_version())
         f.close()
 
@@ -226,84 +108,62 @@ class build_doc(build.build):
         # Build html output.
         #
         if self.html:
-            if newer_group(source_files, 'doc/html'):
+            if newer_group(source_files, 'html/tutorial'):
                 self.announce("building html tutorial")
-                if os.path.isdir('doc/html'): remove_tree('doc/html')
-                self.mkpath('doc/html')
-                cmd = [xsltproc, '--novalid', '--xinclude',
-                       '-o', 'doc/html/',
-                       'doc/html.xsl', 'doc/tutorial.xml']
+                if os.path.isdir('html/tutorial'): remove_tree('html/tutorial')
+                self.mkpath('tutorial/html')
+                cmd = xsltproc.split() + ['--novalid', '--xinclude',
+                                          '-o', 'html/tutorial/',
+                                          srcdir + '/doc/html.xsl',
+                                          srcdir + '/doc/tutorial.xml']
                 self.announce(' '.join(cmd))
                 spawn(cmd)
-                copy_file('doc/cs.css', 'doc/html/cs.css')
+                copy_file(srcdir + '/doc/cs.css', 'html/tutorial/cs.css')
+            dest = srcdir + '/share/doc/qmtest/html/tutorial'
+            if newer('html/tutorial', dest):
+                rmtree(dest, True)
+                copy_tree('html/tutorial', dest)
 
         #
         # Build pdf output.
         #
         if self.pdf:
-            target = normpath("doc/print/tutorial.tex")
-            if newer_group(source_files, target):
-                self.announce("building tex tutorial")
-                # Remove the target first such that its new mtime reflects
-                # this build.
-                if os.path.isfile(target): os.remove(target)
-                self.call_jade(jade, ['-o','tutorial.tex'], 'tex',
-                               normpath('doc/tutorial.xml'),
-                               normpath('doc'))
-
-                # Jade places the output TeX source file in the current
-                # directory, so move it where we want it afterwards.  We have
-                # to change -- into -{-} so that TeX does not generate long
-                # dashes.  This is a bug in Jade.
-                orig_tex_tutorial = normpath("doc/tutorial.tex")
-                self.mkpath(normpath("doc/print"))
-                self.spawn(['sh', '-c',
-                            ('sed -e "s|--|-{-}|g" < %s > %s'
-                             % (orig_tex_tutorial,
-                                normpath("doc/print/tutorial.tex")))])
-                os.remove(orig_tex_tutorial)
-
-            pdf_file = os.path.join("qm", "test", "doc", "print", "tutorial.pdf")
-            if newer_group(source_files, pdf_file):
+            if newer_group(source_files, 'print/tutorial.pdf'):
                 self.announce("building pdf tutorial")
-                # Remove the pdf_file first such that its new mtime reflects
-                # this build.
-                if os.path.isfile(pdf_file): os.remove(pdf_file)
-                cwd = os.getcwd()
-                os.chdir("doc/print")
-                for i in xrange(3):
-                    self.spawn(['pdfjadetex', "tutorial.tex"])
-                os.chdir(cwd)
+                self.mkpath('print')
+                cmd = xsltproc.split() + ['--novalid', '--xinclude',
+                                          '-o', 'print/tutorial.fo',
+                                          srcdir + '/doc/fo.xsl',
+                                          srcdir + '/doc/tutorial.xml']
+                self.announce(' '.join(cmd))
+                spawn(cmd)
+                if xep:
+                    cmd = foproc.split() + ['print/tutorial.fo']
+                else:
+                    cmd = foproc.split() + ['-o', 'print/tutorial.pdf',
+                                            'print/tutorial.fo']
+                self.announce(' '.join(cmd))
+                spawn(cmd)
+                self.mkpath(srcdir + '/share/doc/qmtest/print')
+            dest = srcdir + '/share/doc/qmtest/print/tutorial.pdf'
+            if newer('print/tutorial.pdf', dest):
+                copy_file('print/tutorial.pdf', dest)
+
+        os.chdir(srcdir)
 
         #
-        # Build reference manual via 'happydoc'.
+        # Build reference manual via 'epydoc' or 'happydoc'.
         #
-        if self.internal:
-            happydoc = find_executable('happydoc')
-            if (happydoc):
+        if self.ref_manual:
+            pydoc = find_executable('epydoc')
+            if pydoc: pydoc += ' --no-sourcecode -o share/doc/qmtest/html/manual'
+
+            if not pydoc:
+                pydoc = find_executable('happydoc')
+                if pydoc: pydoc += ' -d share/doc/qmtest/html/manual'
+
+            if not pydoc:
+                self.warn("could not find either of epydoc or happydoc in PATH")
+            else:
                 self.announce("building reference manual")
-                spawn(['happydoc', 'qm'])
-
-
-    def find_file(self, paths, predicate, description):
-        """Return a file satisfying 'predicate' from 'paths'.
-
-        'paths' -- A sequence of glob patterns.
-        
-        'predicate' -- A callable taking a single string as an argument.
-        
-        returns -- The name of the first file matching one of the 'paths'
-        and 'predicate'."""
-
-        for path in paths:
-            matches = filter(predicate, glob.glob(path))
-            if matches:
-                return matches[0]
-
-        self.warn("could not find %s in:" % description)
-        for p in paths:
-            self.warn("  " + p)
-            
-        return None
-
-
+                spawn(pydoc.split() + ['qm'])
