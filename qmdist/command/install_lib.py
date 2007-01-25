562c5db1a469558ab9c07c7d3020f2d5937c5c5a
Stefan Seefeld <stefan@codesourcery.com>
2007-01-25 16:03:42 +0000

    Attempt to fix race-condition during installation.

--- qmdist/command/install_lib.py
+++ qmdist/command/install_lib.py
@@ -65,10 +65,10 @@ class install_lib(base):
 
         if self.compile:
             byte_compile(files, optimize=0,
-                         force=self.force, prefix=install_root,
+                         force=1, prefix=install_root,
                          dry_run=self.dry_run)
         if self.optimize > 0:
             byte_compile(files, optimize=self.optimize,
-                         force=self.force, prefix=install_root,
+                         force=1, prefix=install_root,
                          verbose=self.verbose, dry_run=self.dry_run)
 
